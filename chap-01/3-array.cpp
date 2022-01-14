dd163ef6e5abb273403343e118012a56f29e3c51
Thomas Saillard <thomas.saillard@protonmail.com>
2022-01-14 14:12:36 +0100

    Finished Course 1

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    int array[] = { 0, 1, 2, 3 };
+int main(int argc, char** argv) {
+    using namespace std;
 
-    for (int i = 0; i < 4; ++i)
+    if (argc < 2)
     {
-        std::cout << array[i] << std::endl;
+        cerr << "Error : Where is the size of the array ?" << endl;
+        return -1;
     }
+
+    int length = stoi(argv[1]);
+    if (length <= 0)
+    { cerr << "The length cannot be null or negative" << endl; }
+
+    vector<int> array;
+
+    for (int i = 0; i < length; i++)
+    { array.emplace_back(i + 1); }
+
+    for (int value : array)
+    { cout << value << endl; }
+
+    // int array[length] = {};
+
+    // for (int i = 0; i < length; i++)
+    // { array[i] = i + 1; }
+
+    // for (int i : array)
+    // { cout << i << endl; }
 
     return 0;
 }
