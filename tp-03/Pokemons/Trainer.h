745dc8f9ad972e44fe975361b91311962170cc0d
Thomas Saillard <thomas.saillard@protonmail.com>
2022-01-28 15:08:26 +0100

    pokemon 8

--- tp-03/Pokemons/Trainer.h
+++ tp-03/Pokemons/Trainer.h
@@ -12,6 +12,17 @@ public:
     std::string name() { return _name; }
 
     std::array<Pokeball, 6>& pokeballs() { return _pokeballs; }
+    void                     capture(std::unique_ptr<Pokemon> pokemon) {
+        for (int i = 0; i < 6; i++)
+        {
+            if (_pokeballs[i].empty())
+            {
+                _pokeballs[i].store(std::move(pokemon));
+                return;
+            }
+        }
+        _pc.receive(std::move(pokemon));
+    }
 
 private:
     std::string             _name;
