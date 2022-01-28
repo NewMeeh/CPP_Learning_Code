b744366b53e3a0a7a0c593304ca196e8ff19468b
Thomas Saillard <thomas.saillard@protonmail.com>
2022-01-28 10:09:36 +0100

    pokemon 3

--- tp-03/Pokemons/Pokeball.h
+++ tp-03/Pokemons/Pokeball.h
@@ -1,8 +1,13 @@
 #pragma once
-
+#include "Pokemon.h"
 // A ball where Pokemon sleep.
 class Pokeball
 {
 public:
-    bool empty() { return true; }
+    bool     empty() { return _pokemon == nullptr; }
+    void     store(std::unique_ptr<Pokemon> pokemon) { _pokemon = std::move(pokemon); }
+    Pokemon& pokemon() { return *_pokemon; }
+
+private:
+    std::unique_ptr<Pokemon> _pokemon;
 };
