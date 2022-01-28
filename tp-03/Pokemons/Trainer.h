8718507b3c629b8707a5fa1ca8d7e3ee0064d658
Thomas Saillard <thomas.saillard@protonmail.com>
2022-01-28 10:29:07 +0100

    pokemon 6

--- tp-03/Pokemons/Trainer.h
+++ tp-03/Pokemons/Trainer.h
@@ -1,5 +1,16 @@
 #pragma once
-
+#include "PC.h"
 // A person that capture Pokemons and make them fight.
 class Trainer
-{};
+{
+public:
+    Trainer(std::string name, PC& pc)
+        : _name { name }
+        , _pc { pc } {}
+
+    std::string name() { return _name; }
+
+private:
+    std::string _name;
+    PC&         _pc;
+};
